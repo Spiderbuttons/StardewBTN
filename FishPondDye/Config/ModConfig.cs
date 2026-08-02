faf3ed6e001a629124d6527dbe6c0af4bba1276a
Spiderbuttons <azurylldmm@gmail.com>
2026-08-01 23:37:59 -0400

    [FishPondDye] Initial commit.

---
+++ FishPondDye/Config/ModConfig.cs
@@ -0,0 +1,36 @@
+﻿using GenericModConfigMenu;
+using StardewModdingAPI;
+
+namespace FishPondDye.Config;
+
+public sealed class ModConfig
+{
+    public bool Enabled { get; set; } = true;
+
+    public ModConfig()
+    {
+        Init();
+    }
+
+    private void Init()
+    {
+        this.Enabled = true;
+    }
+
+    public void SetupConfig(IGenericModConfigMenuApi configMenu, IManifest ModManifest, IModHelper Helper)
+    {
+        configMenu.Register(
+            mod: ModManifest,
+            reset: Init,
+            save: () => Helper.WriteConfig(this)
+        );
+
+        configMenu.AddBoolOption(
+            mod: ModManifest,
+            name: () => "Enabled",
+            tooltip: () => "Enable or disable this mod.",
+            getValue: () => this.Enabled,
+            setValue: value => this.Enabled = value
+        );
+    }
+}
---
+++ FishPondDye/ModEntry.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using GenericModConfigMenu;
+using HarmonyLib;
+using Microsoft.Xna.Framework;
+using FishPondDye.Config;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+using FishPondDye.Helpers;
+
+namespace FishPondDye
+{
+    internal sealed class ModEntry : Mod
+    {
+        internal static IModHelper ModHelper { get; set; } = null!;
+        internal static IMonitor ModMonitor { get; set; } = null!;
+        internal static ModConfig Config { get; set; } = null!;
+        internal static Harmony Harmony { get; set; } = null!;
+
+        public override void Entry(IModHelper helper)
+        {
+            i18n.Init(helper.Translation);
+            ModHelper = helper;
+            ModMonitor = Monitor;
+            Config = helper.ReadConfig<ModConfig>();
+            Harmony = new Harmony(ModManifest.UniqueID);
+
+            Harmony.PatchAll();
+
+            Helper.Events.Input.ButtonPressed += OnButtonPressed;
+            Helper.Events.GameLoop.GameLaunched += OnGameLaunched;
+        }
+
+        private void OnGameLaunched(object? sender, GameLaunchedEventArgs e)
+        {
+            var configMenu = Helper.ModRegistry.GetApi<IGenericModConfigMenuApi>("spacechase0.GenericModConfigMenu");
+            if (configMenu != null) Config.SetupConfig(configMenu, ModManifest, Helper);
+        }
+
+        private void OnButtonPressed(object? sender, ButtonPressedEventArgs e)
+        {
+            if (!Context.IsWorldReady)
+                return;
+        }
+    }
+}
