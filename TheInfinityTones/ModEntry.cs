604c69cad7c0a3105559ecf386b0628fd0515779
Spiderbuttons <azurylldmm@gmail.com>
2026-08-30 23:26:36 -0400

    [TheInfinityTones] Initial commit.

---
+++ TheInfinityTones/ModEntry.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using HarmonyLib;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+using TheInfinityTones.Helpers;
+
+namespace TheInfinityTones
+{
+    internal sealed class ModEntry : Mod
+    {
+        internal static IModHelper ModHelper { get; set; } = null!;
+        internal static IMonitor ModMonitor { get; set; } = null!;
+        internal static Harmony Harmony { get; set; } = null!;
+
+        public override void Entry(IModHelper helper)
+        {
+            i18n.Init(helper.Translation);
+            ModHelper = helper;
+            ModMonitor = Monitor;
+            Harmony = new Harmony(ModManifest.UniqueID);
+
+            Harmony.PatchAll();
+
+            Helper.Events.Input.ButtonPressed += OnButtonPressed;
+        }
+
+        private void OnButtonPressed(object? sender, ButtonPressedEventArgs e)
+        {
+            if (!Context.IsWorldReady)
+                return;
+        }
+    }
+}
