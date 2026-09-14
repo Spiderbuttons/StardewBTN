ef13b9a93745ec7a098ee3830b57c2eb53cc4bd0
Spiderbuttons <azurylldmm@gmail.com>
2026-09-14 16:26:39 -0400

    [StardewModTemplate] Add new dotnet template tailored to this monorepo.

---
+++ StardewModTemplate/Config/ModConfig.cs
@@ -0,0 +1,38 @@
+﻿using StardewModdingAPI;
+using SpiderCore.Common.Integration;
+// ReSharper disable MemberCanBePrivate.Global
+
+namespace StardewModTemplate.Config
+{
+    public sealed class ModConfig
+    {
+        public bool Enabled { get; set; } = true;
+    
+        public ModConfig()
+        {
+            Reset();
+        }
+
+        private void Reset()
+        {
+            Enabled = true;
+        }
+
+        public void CreateConfigMenu(IGenericModConfigMenuApi config, IManifest ModManifest, IModHelper Helper)
+        {
+            config.Register(
+                mod: ModManifest,
+                reset: Reset,
+                save: () => Helper.WriteConfig(this)
+            );
+        
+            config.AddBoolOption(
+                mod: ModManifest,
+                name: () => "Enabled",
+                tooltip: () => "Enable or disable this mod.",
+                getValue: () => Enabled,
+                setValue: value => Enabled = value
+            );
+        }
+    }
+}
---
+++ StardewModTemplate/ModEntry.cs
@@ -0,0 +1,68 @@
+﻿using HarmonyLib;
+#if (EnableConfig)
+using SpiderCore.Common.Integration;
+#endif
+#if (EnableConsoleCommands)
+using SpiderCore.Common.Commands;
+#endif
+using SpiderCore.Common.Logging;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+#if (EnableConfig)
+using StardewModTemplate.Config;
+#endif
+// ReSharper disable MemberCanBePrivate.Global
+
+namespace StardewModTemplate
+{
+    internal sealed class ModEntry : Mod
+    {
+        internal static string UNIQUE_ID => Manifest.UniqueID;
+
+        internal static IManifest Manifest { get; set; } = null!;
+        internal static IModHelper ModHelper { get; set; } = null!;
+        #if (EnableConfig)
+        internal static ModConfig Config { get; set; } = null!;
+        #endif
+        #if (EnableConsoleCommands)
+        internal static CommandHandler CommandHandler { get; set; } = null!;
+        #endif
+        internal static Harmony Harmony { get; set; } = null!;
+
+        public override void Entry(IModHelper helper)
+        {
+            Log.Monitor = Monitor;
+            Manifest = ModManifest;
+            ModHelper = helper;
+            #if (EnableTranslations)
+            i18n.Init(ModHelper.Translation);
+            #endif
+            #if (EnableConsoleCommands)
+            CommandHandler = new CommandHandler(ModHelper, Manifest, "$(RootCommand)");
+            CommandHandler.Register();
+            #endif
+
+            Harmony = new Harmony(ModManifest.UniqueID);
+            Harmony.PatchAll();
+
+            Helper.Events.Input.ButtonPressed += OnButtonPressed;
+            #if (EnableConfig)
+            Helper.Events.GameLoop.GameLaunched += OnGameLaunched;
+            #endif
+        }
+
+        #if (EnableConfig)
+        private void OnGameLaunched(object? sender, GameLaunchedEventArgs e)
+        {
+            var configMenu = Helper.ModRegistry.GetApi<IGenericModConfigMenuApi>("spacechase0.GenericModConfigMenu");
+            if (configMenu != null) Config.CreateConfigMenu(configMenu, ModManifest, Helper);
+        }
+        
+        #endif
+        private void OnButtonPressed(object? sender, ButtonPressedEventArgs e)
+        {
+            if (!Context.IsWorldReady)
+                return;
+        }
+    }
+}
