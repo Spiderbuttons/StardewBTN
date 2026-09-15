50be89bd72093c0afdf28647713cbcf668c88266
Spiderbuttons <azurylldmm@gmail.com>
2026-09-15 03:15:25 -0400

    [UnlimitedChestColours] Initial commit.

---
+++ UnlimitedChestColours/ModEntry.cs
@@ -0,0 +1,38 @@
+﻿using HarmonyLib;
+using SpiderCore.Common.Logging;
+using SpiderCore.Common.Shaders;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+
+// ReSharper disable MemberCanBePrivate.Global
+
+namespace UnlimitedChestColours
+{
+    internal sealed class ModEntry : Mod
+    {
+        internal static string UNIQUE_ID => Manifest.UniqueID;
+
+        internal static IManifest Manifest { get; set; } = null!;
+        internal static IModHelper ModHelper { get; set; } = null!;
+        internal static Harmony Harmony { get; set; } = null!;
+
+        public override void Entry(IModHelper helper)
+        {
+            Log.Monitor = Monitor;
+            Manifest = ModManifest;
+            ModHelper = helper;
+            ShaderUtilities.Helper = ModHelper;
+
+            Harmony = new Harmony(ModManifest.UniqueID);
+            Harmony.PatchAll();
+
+            Helper.Events.Input.ButtonPressed += OnButtonPressed;
+        }
+
+        private void OnButtonPressed(object? sender, ButtonPressedEventArgs e)
+        {
+            if (!Context.IsWorldReady)
+                return;
+        }
+    }
+}
